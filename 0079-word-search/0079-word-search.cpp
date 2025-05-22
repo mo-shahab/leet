1296528a4ef97817559168d537e399419b7326e7
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2025-05-22 19:30:28 +0530

    0079-WORD-SEARCH Stats: Time: 209 ms (77.52%), Space: 10.8 MB (62.77%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -6,44 +6,56 @@ public:
     vector<vector<char>> _board;
     string _word;
 
-    bool isValid(int row, int col) {
-        if (row < 0 || row >= n)
-            return false;
-        if (col < 0 || col >= m)
-            return false;
+    bool isValid(int row, int col)
+    {
+        if (row < 0 || row >= n) return false;
+        if (col < 0 || col >= m) return false;
         return true;
     }
 
-    bool dfs(int row, int col, int ptr) {
-        if (ptr == _word.size())
-            return true;
-        
-        if (!isValid(row, col) || _board[row][col] == '0' || _word[ptr] != _board[row][col])
+    bool dfs(
+        int row, 
+        int col, 
+        int ptr
+    )
+    {
+        if(ptr == _word.size()) return true;
+
+        if(
+            !isValid(row, col) || 
+            _board[row][col] == '0' || 
+            _word[ptr] != _board[row][col]
+        ) 
             return false;
 
         char temp = _board[row][col];
         _board[row][col] = '0';
 
-        // 4 possible directions that this can happen
-        for (int i = 0; i < 4; ++i) {
-            if(dfs(row + dr[i], col + dc[i], ptr+1))
-                return true;
+        for (int i = 0; i < 4; ++i) 
+        {
+            if(dfs(row + dr[i], col + dc[i], ptr+1)) return true;
+                
         }
 
         _board[row][col] = temp;
         return false;
+
     }
 
-    bool exist(vector<vector<char>>& board, string word) {
-        // row size and col size
+
+    bool exist(vector<vector<char>>& board, string word) 
+    {
         n = board.size(), m = board[0].size();
         _board = board;
         _word = word;
 
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                if (dfs(i, j, 0))
-                    return true;
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++){
+                // here i is the row, j is the col and 
+                // 0 is the pointer for the word, which means 
+                // first letter of the word
+                if (dfs(i, j, 0)) return true;
             }
         }
 
