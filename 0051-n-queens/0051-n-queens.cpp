c46c96601dc0be823b25745822a52ac6cdea5703
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2025-05-23 20:21:36 +0530

    0051-N-QUEENS Stats: Time: 2 ms (75.98%), Space: 10.2 MB (54.95%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    vector<vector<string>> solveNQueens(int n) {
+    vector<vector<string>> solveNQueens(int n) 
+    {
         vector<vector<string>> result;
         if(n <= 0) return {{}};
         vector<string> board(n, string(n, '.'));
@@ -8,14 +9,18 @@ public:
         return result;
     }
 
-    void dfs(vector<vector<string>>& result, vector<string>& board, int row){
-        if(row == board.size()){
+    void dfs(vector<vector<string>>& result, vector<string>& board, int row)
+    {
+        if(row == board.size())
+        {
             result.push_back(board);
             return;
         }
 
-        for(int i = 0; i < board.size(); i++){
-            if(is_valid(board, row, i)){
+        for(int i = 0; i < board.size(); i++)
+        {
+            if(is_valid(board, row, i))
+            {
                 board[row][i] = 'Q';
                 dfs(result, board, row + 1);
                 board[row][i] = '.';
@@ -23,16 +28,20 @@ public:
         }
     }
 
-    bool is_valid(vector<string>& board, int row, int col){
-        for(int i = row; i >= 0; i--){
+    bool is_valid(vector<string>& board, int row, int col)
+    {
+        for(int i = row; i >= 0; i--)
+        {
             if(board[i][col] == 'Q') return false;
         }
 
-        for(int i = row, j= col; i >= 0 && j >= 0; i--, j--){
-            if(board[i][j] == 'Q') return false;
+        for(int i = row, j = col; i>=0 && j>=0; i--, j--)
+        {
+            if(board[i][j] == 'Q')  return false;
         }
 
-        for(int i = row, j = col; i >= 0 && j < board.size(); i--, j++){
+        for(int i = row, j = col; i>=0 && j < board.size(); i--, j++)
+        {
             if(board[i][j] == 'Q') return false;
         }
 
