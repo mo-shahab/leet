c0a8a939b282aa1d7aa5315dbeaf929f78fd3122
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2025-05-23 21:09:06 +0530

    0051-N-QUEENS Stats: Time: 3 ms (66.23%), Space: 10.2 MB (74.97%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -19,7 +19,7 @@ public:
 
         for(int i = 0; i < board.size(); i++)
         {
-            if(is_valid(board, row, i))
+            if(isValid(board, row, i))
             {
                 board[row][i] = 'Q';
                 dfs(result, board, row + 1);
@@ -28,19 +28,19 @@ public:
         }
     }
 
-    bool is_valid(vector<string>& board, int row, int col)
+    bool isValid(vector<string>& board, int row, int col)
     {
         for(int i = row; i >= 0; i--)
         {
             if(board[i][col] == 'Q') return false;
         }
 
-        for(int i = row, j = col; i>=0 && j>=0; i--, j--)
+        for(int i = row, j = col; i >= 0 && j >= 0; i--, j--)
         {
-            if(board[i][j] == 'Q')  return false;
+            if(board[i][j] == 'Q') return false;
         }
 
-        for(int i = row, j = col; i>=0 && j < board.size(); i--, j++)
+        for(int i = row, j = col; i >= 0 && j < board.size(); i--, j++)
         {
             if(board[i][j] == 'Q') return false;
         }
