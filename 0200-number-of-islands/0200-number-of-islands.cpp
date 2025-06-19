5009b13c8a0f2f09e16a71e1ccd6ce215f7e610d
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2025-06-19 11:29:34 +0530

    0200-NUMBER-OF-ISLANDS Stats: Time: 33 ms (25.9%), Space: 23.7 MB (28.12%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -2,50 +2,45 @@ class Solution {
 public:
     int dRow[4] = {0, -1, 0, 1};
     int dCol[4] = {-1, 0, 1, 0};
-
-    int numIslands(vector<vector<char>>& grid) 
-    {
+    int numIslands(vector<vector<char>>& grid) {
         if(grid.empty()) return 0;
 
         int rows = grid.size();
         int cols = grid[0].size();
-        
+
         vector<vector<bool>> vis(rows, vector<bool>(cols, false));
         int islands = 0;
 
-        for(int row = 0; row < rows; row++)
-        {
-            for(int col = 0; col < cols; col++)
-            {
-                if(grid[row][col] == '1' && (!vis[row][col]))
-                {
+        for(int row = 0; row < rows; row++){
+            for(int col = 0; col < cols; col++){
+                if(grid[row][col] == '1' && !vis[row][col]){
                     bfs(grid, vis, row, col, rows, cols);
                     islands++;
                 }
             }
         }
+
         return islands;
     }
-    
-    void bfs(vector<vector<char>>& grid, vector<vector<bool>>& vis, int row, int col, int rows, int cols)
-    {
+
+    void bfs(vector<vector<char>>& grid, vector<vector<bool>>& vis, int row, int col, int rows, int cols){
+
         queue<pair<int, int>> q;
         q.push({row, col});
         vis[row][col] = true;
 
-        while(!q.empty())
-        {
+        while(!q.empty()){
             pair<int, int> curr = q.front();
+            q.pop();
+            
             int row = curr.first;
             int col = curr.second;
 
-            q.pop();
 
-            for(int i = 0; i < 4; i++)
-            {
+            for(int i = 0; i < 4; i++){
                 int adjx = row + dRow[i];
                 int adjy = col + dCol[i];
-                
+
                 if(adjx >= 0 && adjx < rows && adjy >= 0 && adjy < cols)
                 {
                     if(!vis[adjx][adjy] && grid[adjx][adjy] == '1')
@@ -53,8 +48,7 @@ public:
                         q.push({adjx, adjy});
                         vis[adjx][adjy] = true;
                     }
-                }
-            }
+                }            }
         }
     }
 };
