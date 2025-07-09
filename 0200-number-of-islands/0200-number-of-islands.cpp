a795772e75f510f2c35fdaf92ff9a46196159bca
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2025-07-09 14:47:30 +0530

    0200-NUMBER-OF-ISLANDS Stats: Time: 31 ms (33.29%), Space: 23.8 MB (27.73%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,52 +3,59 @@ public:
     int dRow[4] = {0, -1, 0, 1};
     int dCol[4] = {-1, 0, 1, 0};
     int numIslands(vector<vector<char>>& grid) {
-        if(grid.empty()) return 0;
-
         int rows = grid.size();
         int cols = grid[0].size();
+        int count = 0;
 
         vector<vector<bool>> vis(rows, vector<bool>(cols, false));
-        int islands = 0;
 
+        // lets write a for loop properly 
         for(int row = 0; row < rows; row++){
             for(int col = 0; col < cols; col++){
-                if(grid[row][col] == '1' && !vis[row][col]){
-                    bfs(grid, vis, row, col, rows, cols);
-                    islands++;
+                if(
+                    grid[row][col] == '1' && !vis[row][col]
+                ){
+                    bfs(grid, row, col, rows, cols, vis); 
+                    count++;
                 }
             }
         }
 
-        return islands;
+        return count;
     }
 
-    void bfs(vector<vector<char>>& grid, vector<vector<bool>>& vis, int row, int col, int rows, int cols){
+    void bfs(vector<vector<char>> &grid, int &row, int &col, int &rows, int &cols, vector<vector<bool>> &vis) {
 
         queue<pair<int, int>> q;
         q.push({row, col});
         vis[row][col] = true;
 
         while(!q.empty()){
-            pair<int, int> curr = q.front();
-            q.pop();
-            
+            auto& curr = q.front();
+
             int row = curr.first;
             int col = curr.second;
 
+            q.pop();
+
+            vis[row][col] = true;
 
             for(int i = 0; i < 4; i++){
                 int adjx = row + dRow[i];
                 int adjy = col + dCol[i];
 
-                if(adjx >= 0 && adjx < rows && adjy >= 0 && adjy < cols)
-                {
-                    if(!vis[adjx][adjy] && grid[adjx][adjy] == '1')
-                    {
-                        q.push({adjx, adjy});
-                        vis[adjx][adjy] = true;
-                    }
-                }            }
+                if(
+                    adjx >= 0 && 
+                    adjy >= 0 && 
+                    adjx < rows && 
+                    adjy < cols && 
+                    !vis[adjx][adjy] && 
+                    grid[adjx][adjy] == '1'
+                    ){
+                    q.push({adjx, adjy});
+                    vis[adjx][adjy] = true;
+                }
+            }
         }
     }
 };
