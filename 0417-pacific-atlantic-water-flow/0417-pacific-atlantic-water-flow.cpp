4930bff42b06c8b6c709198c7f35e6391fabed78
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2025-07-19 16:49:33 +0530

    0417-PACIFIC-ATLANTIC-WATER-FLOW Stats: Time: 5 ms (74.12%), Space: 24.2 MB (42.38%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -3,30 +3,25 @@ public:
     int dRow[4] = {0, -1, 0, 1};
     int dCol[4] = {-1, 0, 1, 0};
 
-    void bfs(vector<vector<int>>& h, queue<pair<int, int>>& q, vector<vector<int>>& vis)
+    void bfs(vector<vector<int>> &height, vector<vector<int>> &vis, int rows, int cols, queue<pair<int, int>> &q)
     {
-        int rows = h.size();
-        int cols = h[0].size();
-
-        while(!q.empty())
-        {
+        while (!q.empty()){
             auto curr = q.front();
             int row = curr.first;
             int col = curr.second;
 
             q.pop();
+
             vis[row][col] = 1;
 
-            for(int i{}; i < 4; i++)
-            {
+            for(int i = 0; i < 4; i++){
                 int adjx = row + dRow[i];
                 int adjy = col + dCol[i];
 
-                if(adjx >= 0 && adjy >= 0 && adjx < rows && adjy < cols)
-                {
-                    if(h[adjx][adjy] >= h[row][col] && vis[adjx][adjy] == 0)
-                    {
+                if(adjx >= 0 && adjy >= 0 && adjx < rows && adjy < cols) {
+                    if(height[adjx][adjy] >= height[row][col] && !vis[adjx][adjy]){
                         q.push({adjx, adjy});
+                        vis[adjx][adjy] = 1;
                     }
                 }
             }
@@ -42,21 +37,22 @@ public:
         vector<vector<int>> pacific(rows, vector<int>(cols, 0));
         vector<vector<int>> atlantic(rows, vector<int>(cols, 0));
 
+
+        queue<pair<int, int>> q;
+        for(int i = 0; i < cols; i++) q.push({0, i});
+        for(int i = 0; i < rows; i++) q.push({i, 0});
+        bfs(heights, pacific, rows, cols, q);
+
         queue<pair<int, int>> q1;
-        for(int i{}; i < rows; i++) q1.push({i, 0});
-        for(int j{}; j < cols; j++) q1.push({0, j});
-        bfs(heights, q1, pacific);
+        for(int i = 0; i < cols; i++) q1.push({rows-1, i});
+        for(int i = 0; i < rows; i++) q1.push({i, cols-1});
+        bfs(heights, atlantic, rows, cols, q1);
 
-        queue<pair<int, int>> q2;
-        for(int i{}; i < rows; i++) q2.push({i, cols - 1});
-        for(int j{}; j < cols; j++) q2.push({rows - 1, j});
-        bfs(heights, q2, atlantic);
-
-        for(int i{}; i < rows; i++)
-        {
-            for(int j{}; j < cols; j++)
-            {
-                if(pacific[i][j] == 1 && atlantic[i][j] == 1) result.push_back({i, j});
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(pacific[i][j] == atlantic[i][j] && pacific[i][j] == 1) {
+                    result.push_back({i, j});
+                }
             }
         }
 
