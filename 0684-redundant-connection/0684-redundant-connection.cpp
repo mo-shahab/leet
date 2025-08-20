04db99d0bae09e032fdae70d4b0fa12ba7b5622b
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2025-08-20 17:44:53 +0530

    0684-REDUNDANT-CONNECTION Stats: Time: 0 ms (100%), Space: 12.8 MB (83.72%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,40 +1,45 @@
-class UnionFind { // usual UnionFind class
+class UnionFind {
     vector<int> root, rank;
 public:
-    UnionFind(int n) : root(n), rank(n) {
+    UnionFind(int n){
         rank.assign(n, 1);
+        root.resize(n); // Corrected: Resize the root vector
         iota(root.begin(), root.end(), 0);
     }
 
-    int Find(int x) {//Path compression
-        if (x == root[x])
-            return x;
-        else
-            return root[x] = Find(root[x]);
+    int Find(int x){
+        if(x == root[x]) return x;
+        else return root[x] = Find(root[x]);
     }
 
-    bool Union(int x, int y) {//Union by rank with connecting info
-        int rX = Find(x), rY = Find(y);
-        //they have same root, already in same component
-        if (rX == rY)
-            return 0;// does not affect the components
-        if (rank[rX] > rank[rY])
-            swap(rX, rY);
-        root[rX] = rY;
-        if (rank[rX] == rank[rY])
-            rank[rY]++;
-        return 1;// connect 2 disjoint components
+    bool Union(int x, int y) {
+        int rootX = Find(x);
+        int rootY = Find(y);
+    
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                root[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                root[rootX] = rootY;
+            } else {
+                root[rootY] = rootX;
+                rank[rootX]++;
+            }
+            return true;
+        }
+        return false;
     }
 };
 
 class Solution {
 public:
+
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+
         const int n=edges.size();
         UnionFind G(n+1);
         for(auto& e: edges)
             if (!G.Union(e[0], e[1])) return e;
-        return {};
+        return {}; 
     }
 };
-
