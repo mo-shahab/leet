92a486fcc50662445cb9fdff1f6a9db5410c4232
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2025-10-31 20:55:33 +0530

    0045-JUMP-GAME-II Stats: Time: 0 ms (100%), Space: 20.3 MB (98.03%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,25 +2,31 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
 
-        int res = 0, l = 0, r = 0;
+        int jumps = 0;              // Counts the number of jumps taken (our result)
+        int current_jump_start = 0; // The start of the current range to explore
+        int current_jump_end = 0;   // The end of the current range to explore (farthest reach from the previous jump)
 
-        while(r < nums.size() - 1)
+       while(current_jump_end < nums.size() - 1)
         {
-            int reach = 0;
+            // 1. Initialize for the *next* jump's potential
+            int farthest_reach = 0;
 
-            for(int i=l; i < r+1; i++)
+            // 2. Explore the current jump's range (Greedy Choice)
+            for(int i = current_jump_start; i <= current_jump_end; i++)
             {
-                reach = max(reach, i + nums[i]);
+                // Calculate the farthest position we can reach from the current index 'i'
+                // and update our overall best reach within this range.
+                farthest_reach = max(farthest_reach, i + nums[i]);
             }
 
-            l = r + 1;
-            r = reach;
+            // 3. Prepare for the *next* jump (Greedy Jump Taken)
+            current_jump_start = current_jump_end + 1; // The next jump will start *after* the current range
+            current_jump_end = farthest_reach;         // The new range ends at the farthest point we could reach
 
-            res++;
-
+            // 4. Increment the jump count
+            jumps++;
         }
         
-        return res;
-        // see this is how it will be there right ?? 
+        return jumps; 
     }
 };
