c9e18d545685358358c12aac0979d6071faa8ec3
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2025-11-05 21:28:11 +0530

    2026-MERGE-TRIPLETS-TO-FORM-TARGET-TRIPLET Stats: Time: 75 ms (14.27%), Space: 167.7 MB (18.54%) - LeetHub

--- 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.cpp
+++ 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.cpp
@@ -2,18 +2,14 @@ class Solution {
 public:
     bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
 
-        unordered_set<int> st;
+        vector<int> merge{0, 0, 0};
 
         for(auto triplet: triplets)
         {
             if(triplet[0] > target[0] || triplet[1] > target[1] || triplet[2] > target[2]) continue;
-
-            for(int j=0; j<3; j++) 
-            {
-                if(triplet[j] == target[j]) st.insert(j);
-            }
+            merge = {max(merge[0], triplet[0]), max(merge[1], triplet[1]), max(merge[2], triplet[2])};
         }
 
-        return st.size() == 3;
+        return merge == target;
     }
 };
