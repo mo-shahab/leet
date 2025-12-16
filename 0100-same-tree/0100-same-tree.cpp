7427d10d98daab4b0acefcc7c856c1a76f7f5549
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2025-12-16 17:35:57 +0530

    0100-SAME-TREE Stats: Time: 0 ms (100%), Space: 12.9 MB (14.28%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,13 +12,16 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(!p && !q) return true;
+        if(!p || !q) return false;
+
         queue<TreeNode*> q1;
         queue<TreeNode*> q2;
 
         q1.push(p);
         q2.push(q);
 
-        while(!q1.empty() || !q2.empty()){
+        while(!q1.empty() && !q2.empty()){
 
             TreeNode* curr1 = q1.front();
             TreeNode* curr2 = q2.front();
@@ -26,20 +29,25 @@ public:
             q1.pop();
             q2.pop();
 
+            if(curr1->val != curr2->val) return false;
 
-            if(curr1->left && curr2->left && curr1->left == curr2->left) {
+            if(curr1->left && curr2->left) {
                 q1.push(curr1->left);
                 q2.push(curr2->left);
-            } else {
+            } else if (curr1->left || curr2->left){
                 return false;
+            }
+            else {
             }
 
 
-            if(curr1->right && curr2->right && curr1->right == curr2->right) {
+            if(curr1->right && curr2->right) {
                 q1.push(curr1->right);
                 q2.push(curr2->right);
-            } else {
+            } else if (curr1->right || curr2->right){
                 return false;
+            }
+            else {
             }
         }
 
