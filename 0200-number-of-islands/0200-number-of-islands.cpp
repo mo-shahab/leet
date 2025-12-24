fd1afc7fd0bb72aa9ac95ac0facda82b45730421
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2025-12-24 14:28:50 +0530

    0200-NUMBER-OF-ISLANDS Stats: Time: 28 ms (49.42%), Space: 16.7 MB (57.96%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,51 +3,46 @@ public:
     int dx[4] = {0, 0, -1, 1};
     int dy[4] = {1, -1, 0, 0};
 
-    typedef vector<vector<char>> graph;
     typedef vector<vector<bool>> visited;
+    typedef vector<vector<char>> graph; 
 
     int numIslands(vector<vector<char>>& grid) {
         int rows = grid.size();
         int cols = grid[0].size();
         int count = 0;
 
-        vector<vector<bool>> vis(rows, vector<bool> (cols, false));
+        visited vis(rows, vector<bool>(cols, false));
 
-        for(int i = 0; i< rows; i++) {
+        for(int i = 0; i < rows; i++) {
             for(int j = 0; j < cols; j++) {
-                if(grid[i][j]  == '1' && !vis[i][j]) {
-                    bfs(grid, vis, i, j, rows, cols);
+                if(!vis[i][j] && grid[i][j] == '1') {
+                    dfs(grid, vis, i, j, rows, cols);
                     count++;
                 }
             }
         }
 
         return count;
+
     }
 
-    void bfs(graph& grid, visited& vis, int& row, int& col, int& rows, int& cols) {
-        queue<pair<int, int>> q;
-        q.push({row, col});
-
+    void dfs(graph& grid, visited& vis, int row, int col, int& rows, int&cols) {
         vis[row][col] = true;
 
-        while(!q.empty()) {
-            auto curr = q.front();
+        for(int i = 0; i < 4; i++) {
+            int adjx = row + dx[i];
+            int adjy = col + dy[i];
 
-            int curr_row = curr.first;
-            int curr_col = curr.second;
-
-            q.pop();
-            vis[curr_row][curr_col]= true;
-
-            for(int i = 0; i < 4; i++) {
-                int adjx = curr_row + dx[i];
-                int adjy = curr_col + dy[i];
-
-                if(adjx >= 0 && adjy >= 0 && adjx < rows && adjy < cols && !vis[adjx][adjy] && grid[adjx][adjy] == '1') {
-                    q.push({adjx, adjy});
-                    vis[adjx][adjy] = true;
-                }
+            if(
+                adjx >= 0 &&
+                adjy >= 0 &&
+                adjx < rows && 
+                adjy < cols && 
+                !vis[adjx][adjy] && 
+                grid[adjx][adjy] == '1'
+            ) {
+                vis[adjx][adjy] = true;
+                dfs(grid, vis, adjx, adjy, rows, cols);
             }
         }
     }
