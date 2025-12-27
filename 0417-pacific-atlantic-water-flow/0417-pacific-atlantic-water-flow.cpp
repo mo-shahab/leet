7c46dee9bb948f1d2dee26ccb87f57031003aaa4
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2025-12-27 19:11:44 +0530

    0417-PACIFIC-ATLANTIC-WATER-FLOW Stats: Time: 4 ms (79.75%), Space: 24.8 MB (28.09%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,56 +1,71 @@
 class Solution {
 public:
-    int dRow[4] = {0, -1, 0, 1};
-    int dCol[4] = {-1, 0, 1, 0};
+    int dx[4] = {0, 0, -1, 1};
+    int dy[4] = {1, -1, 0, 0};
 
-    void bfs(vector<vector<int>> &height, vector<vector<int>> &vis, int rows, int cols, queue<pair<int, int>> &q)
-    {
-        while (!q.empty()){
+    typedef vector<vector<int>> graph;
+
+    void bfs(graph& grid, graph& vis, queue<pair<int, int>>& q,  int row, int col, int& rows, int&cols) {
+        while(!q.empty()) { 
             auto curr = q.front();
-            int row = curr.first;
-            int col = curr.second;
+            
+            int curr_row = curr.first;
+            int curr_col = curr.second;
+
+            vis[curr_row][curr_col] = 1;
 
             q.pop();
 
-            vis[row][col] = 1;
+            for(int i = 0; i < 4; i++) {
+                int adjx = curr_row + dx[i];
+                int adjy = curr_col + dy[i];
 
-            for(int i = 0; i < 4; i++){
-                int adjx = row + dRow[i];
-                int adjy = col + dCol[i];
-
-                if(adjx >= 0 && adjy >= 0 && adjx < rows && adjy < cols) {
-                    if(height[adjx][adjy] >= height[row][col] && !vis[adjx][adjy]){
-                        q.push({adjx, adjy});
-                        vis[adjx][adjy] = 1;
-                    }
+                if(
+                    adjx >= 0 &&
+                    adjy >= 0 && 
+                    adjx < rows && 
+                    adjy < cols && 
+                    grid[adjx][adjy] >= grid[curr_row][curr_col] && 
+                    vis[adjx][adjy] != 1
+                ) { 
+                    q.push({adjx, adjy});
                 }
             }
+
         }
     }
 
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        vector<vector<int>> result;    
-
         int rows = heights.size();
         int cols = heights[0].size();
 
-        vector<vector<int>> pacific(rows, vector<int>(cols, 0));
-        vector<vector<int>> atlantic(rows, vector<int>(cols, 0));
-
-
-        queue<pair<int, int>> q;
-        for(int i = 0; i < cols; i++) q.push({0, i});
-        for(int i = 0; i < rows; i++) q.push({i, 0});
-        bfs(heights, pacific, rows, cols, q);
-
+        // queue used to run multi source bfs on all the edge nodes
+        // that can reach the pacific;
         queue<pair<int, int>> q1;
-        for(int i = 0; i < cols; i++) q1.push({rows-1, i});
-        for(int i = 0; i < rows; i++) q1.push({i, cols-1});
-        bfs(heights, atlantic, rows, cols, q1);
 
-        for(int i = 0; i < rows; i++){
+        // same but will check for 
+        queue<pair<int, int>> q2;
+
+        for(int i = 0; i < rows; i++) {
             for(int j = 0; j < cols; j++){
-                if(pacific[i][j] == atlantic[i][j] && pacific[i][j] == 1) {
+                if(i == 0 || j == 0) q1.push({i, j});
+                if(i == rows-1 || j == cols-1) q2.push({i, j});
+            }
+        }
+
+        // all the vertices that can reach to pacific
+        graph pacific(rows, vector<int>(cols, 0));
+        bfs(heights, pacific, q1, 0, 0, rows, cols);
+
+        // all the vertices that can reach to atlantic
+        graph atlantic(rows, vector<int>(cols, 0));
+        bfs(heights, atlantic, q2, 0, 0, rows, cols);
+        
+        vector<vector<int>> result; 
+
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                if(pacific[i][j] && atlantic[i][j]) {
                     result.push_back({i, j});
                 }
             }
