ea1cc693d7fbe8779ecc09f22f0f877153c0517d
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2025-12-28 23:04:14 +0530

    0130-SURROUNDED-REGIONS Stats: Time: 0 ms (100%), Space: 14.8 MB (33.42%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    int dRow[4] = {-1, 0, 0, 1};
-    int dCol[4] = {0, -1, 1, 0};
+    int dx[4] = {0, 0, -1, 1};
+    int dy[4] = {1, -1, 0, 0};
+
     void solve(vector<vector<char>>& board) {
         int rows = board.size();
         int cols = board[0].size();
 
-        vector<vector<bool>> vis(rows, vector(cols, false));
-
-        // phase 1 marking all the unsurrounded regions 
-        // as 'T'
         queue<pair<int, int>> q;
-        for(int i = 0; i < rows; i++){
-            if(board[i][0] == 'O'){
+
+        vector<vector<bool>> vis(rows, vector<bool>(cols, false));
+
+        // marking all the Os at the border as 'T'
+        for(int i = 0; i < rows; i++) {
+            if(board[i][0] == 'O') {
                 q.push({i, 0});
                 board[i][0] = 'T';
                 vis[i][0] = true;
             }
-            if(board[i][cols-1] == 'O'){
-                q.push({i, cols-1});
+            if(board[i][cols-1] == 'O') {
+                q.push({i, cols-1}) ;
                 board[i][cols-1] = 'T';
                 vis[i][cols-1] = true;
             }
         }
 
-        for(int i = 0; i < cols; i++){
+        for(int i = 0; i < cols; i++) {
             if(board[0][i] == 'O'){
                 q.push({0, i});
                 board[0][i] =  'T';
@@ -34,46 +35,51 @@ public:
                 q.push({rows-1, i});
                 board[rows-1][i] = 'T';
                 vis[rows-1][i] = true;
-            }
+            }    
         }
 
-        while(!q.empty()){
-            pair<int, int> curr = q.front();
+        while(!q.empty()) {
+            auto curr = q.front();
 
-            int row =  curr.first;
-            int col = curr.second;
+            int curr_row = curr.first;
+            int curr_col = curr.second;
 
             q.pop();
 
-            for(int i = 0; i < 4; i++){
-                int adjx = row + dRow[i];
-                int adjy = col + dCol[i];
+            // these are all the Os which are connected to the border and stuff
+            for(int j = 0; j < 4; j++ ) {
+                int adjx = dx[j] + curr_row;
+                int adjy = dy[j] + curr_col;
 
-                if(adjx >= 0 && adjy >= 0 && adjx < rows && adjy < cols && !vis[adjx][adjy]){
-                    if(board[adjx][adjy] == 'O'){
-                        board[adjx][adjy] = 'T';
-                        vis[adjx][adjy] = true;
-                        q.push({adjx, adjy});
-                    }
+                if(
+                    adjx >= 0 &&
+                    adjy >= 0 &&
+                    adjx < rows && 
+                    adjy < cols && 
+                    !vis[adjx][adjy] &&
+                    board[adjx][adjy] == 'O'
+                ) {
+                    q.push({adjx, adjy});
+                    board[adjx][adjy] = 'T';
+                    vis[adjx][adjy] = true;
                 }
             }
         }
 
         for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++){
+            for(int j = 0; j< cols;j++) {
                 if(board[i][j] == 'O') {
                     board[i][j] = 'X';
                 }
             }
         }
 
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                if(board[i][j] == 'T'){
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                if(board[i][j] == 'T') {
                     board[i][j] = 'O';
                 }
             }
         }
-
     }
 };
