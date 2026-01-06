9061fc2e262d2266678a5eb04a05d1b3e4f1f8e5
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2026-01-06 19:04:35 +0530

    0424-LONGEST-REPEATING-CHARACTER-REPLACEMENT Stats: Time: 5 ms (50.83%), Space: 10.9 MB (38.73%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int start = 0, end = 0;
-        vector<int> count(128, 0);
-        int maxFreq = 0;
-        int result = 0;
+        
+        // in the problems like this 
 
-        for(; end < s.length(); end++){
-            count[s[end]]++;
-            maxFreq = max(maxFreq, count[s[end]]);
-            if((end - start + 1) - maxFreq > k){
-                count[s[start]]--;
+        // dont do any operations on the item just iterate over them and solve like you would normally do 
+        
+
+        // in this case just find the window + k 
+        // keep track of the maximum frequency item so that 
+        // basically y ou have to replace the items less than those maximum frequency ones and stuff 
+
+        int start = 0, end = 0;
+
+        vector<int> freq(128, 0); // because in here you are finding the frequency of the stuff and all 
+
+        int max_freq = 0;
+        int res = 0;
+
+        for(; end < s.size(); end++) {
+            freq[s[end]]++;
+            max_freq = max(max_freq, freq[s[end]]);
+
+            if((end - start + 1) - max_freq > k) {
+                
+                freq[s[start]]--; // reducing the size of the window
                 start++;
             }
 
-            result = max(result, (end-start + 1));
+            res = max(res, (end - start + 1));
         }
 
-        return result;
+        return res;
     }
 };
