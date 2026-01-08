5cc17456151fd739b4d75de513e52870b530cf79
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2026-01-08 14:11:06 +0530

    0003-LONGEST-SUBSTRING-WITHOUT-REPEATING-CHARACTERS Stats: Time: 2 ms (82.04%), Space: 10.9 MB (93.37%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -2,24 +2,38 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int i = 0;
-        int j = 0;    
-        int maxLen = 0;
-         
+        int j = 0;     
 
-        vector<int> freq(128, -1);
+        vector<int> counts(128, 0);
+
+        int max_length = 0;
 
         while(j < s.size()) {
-            if(freq[s[j]] < i) {
-                freq[s[j]] = j;
-                j++;
-            } else {
-                i = freq[s[j]] + 1;
-                freq[s[j]] = j;
-                j++;
+            counts[s[j]]++; // aggresively increase the right pointer no matter what 
+
+            // now other condition comes in here 
+            while(counts[s[j]] > 1) {
+                counts[s[i]]--;
+                i++;
             }
-            maxLen = max(maxLen, j - i);
+
+            max_length = max(max_length, j - i + 1);
+            j++;
         }
 
-        return maxLen;
+        return max_length;
     }
 };
+
+/*
+int lengthOfLongestSubstringTwoDistinct(string s) {
+        vector<int> map(128, 0);
+        int counter=0, begin=0, end=0, d=0; 
+        while(end<s.size()){
+            if(map[s[end++]]++==0) counter++;
+            while(counter>2) if(map[s[begin++]]--==1) counter--;
+            d=max(d, end-begin);
+        }
+        return d;
+    }
+*/
