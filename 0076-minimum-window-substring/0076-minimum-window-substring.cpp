e48579e37d6daeb7a6a6361f35ff272c159d49c1
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2026-01-08 14:04:41 +0530

    0076-MINIMUM-WINDOW-SUBSTRING Stats: Time: 0 ms (100%), Space: 11.4 MB (80.93%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,58 +1,36 @@
-// https://leetcode.com/problems/minimum-window-substring/solutions/26808/here-is-a-10-line-template-that-can-solv-lct0
 class Solution {
 public:
     string minWindow(string s, string t) {
-        // Frequency map to track what we need from string 't'
-        vector<int> charCounts(128, 0);
-        for (char c : t) {
-            charCounts[c]++;
-        }
-
         int left = 0;
         int right = 0;
-        int charactersNeeded = t.size();
-        
-        int minWindowLength = INT_MAX;
+        int minLen = INT_MAX; // this will keep the minimum window thing 
+
+        vector<int> counts(128, 0); // counts of each character in the window 
+        int charactersNeeded = t.size(); 
         int startIndex = 0;
 
-        // Phase 1: Expand the window
-        while (right < s.size()) {
-            char currentRight = s[right];
+        for(auto c: t) counts[c]++;
 
-            // If we found a character that's actually in 't', decrement our requirement
-            if (charCounts[currentRight] > 0) {
-                charactersNeeded--;
-            }
-            
-            // Note: We decrement charCounts even for characters NOT in 't'
-            // This creates negative values for extra/unnecessary characters
-            charCounts[currentRight]--;
+        while(right < s.size()) {
+            if(counts[s[right]] > 0) charactersNeeded--; // if the character from t is already in the window then dec that bs. 
+            counts[s[right]]--; // we now have this character in the window
             right++;
 
-            // Phase 2: Shrink the window once it's "valid"
-            while (charactersNeeded == 0) {
-                int currentWindowSize = right - left;
+            // when we come to this , we already have all the characters from t in the window
+            // now we try to see if we can shrink the size of the sliding window
+            while(charactersNeeded == 0) {
 
-                // Update our record for the smallest window
-                if (currentWindowSize < minWindowLength) {
-                    minWindowLength = currentWindowSize;
+                if(minLen > right - left) {
+                    minLen = right - left;
                     startIndex = left;
                 }
 
-                // Move the left pointer to see if we can still stay valid with less
-                char currentLeft = s[left];
-                charCounts[currentLeft]++;
-
-                // If the count goes above 0, it means we just lost a required character
-                if (charCounts[currentLeft] > 0) {
-                    charactersNeeded++;
-                }
-
+                counts[s[left]]++;
+                if(counts[s[left]] > 0) charactersNeeded++;
                 left++;
             }
         }
 
-        // Final check: did we ever find a valid window?
-        return (minWindowLength == INT_MAX) ? "" : s.substr(startIndex, minWindowLength);
+        return minLen == INT_MAX ? "" : s.substr(startIndex, minLen);
     }
 };
