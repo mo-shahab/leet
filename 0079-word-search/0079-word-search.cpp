f098f292edd112b161bbf4bd8434ce1b9a7a6036
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2026-01-16 17:09:10 +0530

    0079-WORD-SEARCH Stats: Time: 352 ms (46.08%), Space: 11.5 MB (48.05%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,64 +1,59 @@
-int dr[]{1,-1,0, 0};       
-int dc[]{0, 0,1,-1,};
+// this is dfs and backtracking type shi
 class Solution {
 public:
-    int n, m;
-    vector<vector<char>> _board;
-    string _word;
 
-    bool isValid(int row, int col)
-    {
-        if (row < 0 || row >= n) return false;
-        if (col < 0 || col >= m) return false;
-        return true;
-    }
+    int dy[4] = {1, -1, 0, 0};
+    int dx[4] = {0, 0, 1, -1};
 
-    bool dfs(
-        int row, 
-        int col, 
-        int ptr
-    )
-    {
-        if(ptr == _word.size()) return true;
+    bool exist(vector<vector<char>>& board, string word) {
 
-        if(
-            !isValid(row, col) || 
-            _board[row][col] == '0' || 
-            _word[ptr] != _board[row][col]
-        ) 
-            return false;
-
-        char temp = _board[row][col];
-        _board[row][col] = '0';
-
-        for (int i = 0; i < 4; ++i) 
-        {
-            if(dfs(row + dr[i], col + dc[i], ptr+1)) return true;
-                
+        for(int i = 0; i < board.size(); i++) {
+            for(int j = 0; j < board[0].size(); j++)
+            {
+                if(board[i][j] == word[0]) {
+                    if(backtrack(board, word, i, j, 0))
+                    {
+                        return true;
+                    }
+                }
+            }
         }
 
-        _board[row][col] = temp;
         return false;
 
     }
 
-
-    bool exist(vector<vector<char>>& board, string word) 
+// w_count is the number of the letter of the word that we are trying to match and stuff
+    bool backtrack(vector<vector<char>>& board, string word, int row, int col, int w_count)
     {
-        n = board.size(), m = board[0].size();
-        _board = board;
-        _word = word;
-
-        for(int i = 0; i < n; i++)
+        if(board[row][col] != word[w_count])
         {
-            for(int j = 0; j < m; j++){
-                // here i is the row, j is the col and 
-                // 0 is the pointer for the word, which means 
-                // first letter of the word
-                if (dfs(i, j, 0)) return true;
+            return false;
+        }
+        if(w_count == word.size() - 1) return true;
+
+        char temp = board[row][col];
+        board[row][col] = '.';
+
+        for(int i = 0; i < 4; i++)
+        {
+            int adjx = row + dx[i];
+            int adjy = col + dy[i];
+
+            if(
+                adjx >= 0 &&
+                adjy >= 0 &&
+                adjx < board.size()  &&
+                adjy < board[0].size()  &&
+                board[adjx][adjy] != '.' &&
+                board[adjx][adjy] == word[w_count + 1]
+            )
+            {
+                if(backtrack(board, word, adjx, adjy, w_count + 1)) return true; // catch the thing and stuff 
             }
         }
 
+        board[row][col] = temp;
         return false;
     }
 };
