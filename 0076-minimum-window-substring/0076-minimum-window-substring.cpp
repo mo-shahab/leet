989816c4b39638b8e882c8e25d7fe53c246b8f59
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2026-01-19 09:04:21 +0530

    0076-MINIMUM-WINDOW-SUBSTRING Stats: Time: 4 ms (72.11%), Space: 11.2 MB (93.62%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,36 +1,44 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int left = 0;
-        int right = 0;
-        int minLen = INT_MAX; // this will keep the minimum window thing 
+        vector<int> counts(128, 0);
+        int counter = 0; // will track the characters needed and such
+        int min_window = INT_MAX;
+        int start = 0;
 
-        vector<int> counts(128, 0); // counts of each character in the window 
-        int charactersNeeded = t.size(); 
-        int startIndex = 0;
-
-        for(auto c: t) counts[c]++;
-
-        while(right < s.size()) {
-            if(counts[s[right]] > 0) charactersNeeded--; // if the character from t is already in the window then dec that bs. 
-            counts[s[right]]--; // we now have this character in the window
-            right++;
-
-            // when we come to this , we already have all the characters from t in the window
-            // now we try to see if we can shrink the size of the sliding window
-            while(charactersNeeded == 0) {
-
-                if(minLen > right - left) {
-                    minLen = right - left;
-                    startIndex = left;
-                }
-
-                counts[s[left]]++;
-                if(counts[s[left]] > 0) charactersNeeded++;
-                left++;
-            }
+        for(char c: t) {
+            counts[c]++;
+            counter++;
         }
 
-        return minLen == INT_MAX ? "" : s.substr(startIndex, minLen);
+        int i = 0;
+        int j = 0;
+        
+        while(j < s.size()) {
+            if(counts[s[j]] > 0) {
+                counter--;
+            }
+
+            counts[s[j]]--;
+
+            while(counter == 0) {
+                if(j - i + 1 < min_window)
+                {
+                    min_window = j - i + 1;
+                    start = i;
+                }
+
+                counts[s[i]]++;
+                if(counts[s[i]] > 0) counter++;
+                i++;
+            }
+            
+            j++;
+        }
+
+
+        return min_window == INT_MAX ? "": s.substr(start, min_window);
+
+
     }
 };
