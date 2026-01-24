4c0c51bb6f35a2794ffb06fea02146daae3ec183
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2026-01-24 20:14:13 +0530

    0051-N-QUEENS Stats: Time: 2 ms (74.06%), Space: 10.2 MB (76.09%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,49 +1,49 @@
 class Solution {
 public:
-    vector<vector<string>> solveNQueens(int n) 
-    {
-        vector<vector<string>> result;
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> results;
         if(n <= 0) return {{}};
-        vector<string> board(n, string(n, '.'));
-        dfs(result, board, 0);
-        return result;
+
+        vector<string> board(n , string(n, '.'));
+        dfs(board, results, 0); // pass the rvalue for the row;
+
+        return results;
     }
 
-    void dfs(vector<vector<string>>& result, vector<string>& board, int row)
+    void dfs(vector<string>& board, vector<vector<string>>& results, int row)
     {
-        if(row == board.size())
-        {
-            result.push_back(board);
+        if(row == board.size()){
+            results.push_back(board);
             return;
         }
-
+        
         for(int i = 0; i < board.size(); i++)
         {
-            if(isValid(board, row, i))
-            {
-                board[row][i] = 'Q';
-                dfs(result, board, row + 1);
+            if(isValid(board, row, i)) {
+                board[row][i] = 'Q'; // act upon the decision on that thing 
+                dfs(board, results, row + 1); // then do the same for the next row;
                 board[row][i] = '.';
             }
         }
     }
 
-    bool isValid(vector<string>& board, int row, int col)
-    {
-        for(int i = row; i >= 0; i--)
-        {
+    bool isValid(vector<string>& board, int row, int col) {
+        // checks for rows if any rows at the given col has queen
+        for(int i = row; i >= 0; i--) {
             if(board[i][col] == 'Q') return false;
         }
 
-        for(int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+        // upper left diagonal check
+        for(int i= row, j = col; i >= 0 && j >= 0; i--, j--)
         {
-            if(board[i][j] == 'Q') return false;
+            if(board[i][j]== 'Q') return false; 
         }
 
         for(int i = row, j = col; i >= 0 && j < board.size(); i--, j++)
         {
             if(board[i][j] == 'Q') return false;
         }
+
 
         return true;
     }
