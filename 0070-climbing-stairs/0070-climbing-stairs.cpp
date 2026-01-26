d4f43c6b29c6825f5ea191bb8aeae1600fa18406
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2026-01-26 20:34:55 +0530

    0070-CLIMBING-STAIRS Stats: Time: 0 ms (100%), Space: 8.5 MB (34.54%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int one = 1, two = 1;
+        vector<int> dp(n + 1) ; // create a 1-dp array to store the values
 
-        for(int i = 0; i < n-1; i++){
-            int temp = one;
-            one = one + two;
-            two = temp;
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++)
+        {
+            dp[i] = dp[i - 2] + dp[i-1];
         }
 
-        return one;
+        return dp[n];
     }
 };
