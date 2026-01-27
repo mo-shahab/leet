398be711df4e3e86e2d19e87da3ac4b5352c2f48
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2026-01-27 22:21:11 +0530

    0747-MIN-COST-CLIMBING-STAIRS Stats: Time: 0 ms (100%), Space: 17.5 MB (73.62%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        vector<int> dp(cost.size() + 2 );
+        int n = cost.size();
+        vector<int> dp(n);
 
-        dp[cost.size()] = 0;
-        for(int i = cost.size()-1; i >= 0; i--)
-        {
-            dp[i] = cost[i] + min(dp[i+1], dp[i+2]);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for(int i = 2; i < n; i++) {
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
         }
 
-        return min(dp[0], dp[1]);
+        return min(dp[n-1], dp[n-2]);
     }
 };
