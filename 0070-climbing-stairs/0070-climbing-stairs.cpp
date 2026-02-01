a93ec72051b31911bafa7fefaac0d060ddc5b2ce
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2026-02-01 18:32:26 +0530

    0070-CLIMBING-STAIRS Stats: Time: 0 ms (100%), Space: 8.7 MB (17.61%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n + 1) ; // create a 1-dp array to store the values
+        vector<int> dp(n+1);
 
-        dp[0] = 1;
-        dp[1] = 1;
+        dp[0] = 1; // first step
+        dp[1] = 2; // second step
 
-        for(int i = 2; i <= n; i++)
-        {
-            dp[i] = dp[i - 2] + dp[i-1];
+        for(int i = 2; i < n ; i++) {
+            dp[i] = dp[i-1] + dp[i-2]; // its simply how many ways from one prev step and two prev steps
         }
 
-        return dp[n];
+        return dp[n - 1];
     }
 };
