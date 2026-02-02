8f31301f6f5fb4fcbe8c4d2baf5c1765139b1f7e
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2026-02-02 22:44:18 +0530

    0005-LONGEST-PALINDROMIC-SUBSTRING Stats: Time: 82 ms (36.81%), Space: 164.9 MB (14.85%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,33 +1,41 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        if (s.empty()) {
-            return "";
-        }
+        if(s.length() <= 1) return s;
 
         int start = 0;
         int end = 0;
 
-        for (int i = 0; i < s.length(); i++) {
+        for(int i = 0; i < s.length(); i++)
+        {
             int odd = expandAroundCenter(s, i, i);
-            int even = expandAroundCenter(s, i, i + 1);
+            int even = expandAroundCenter(s, i, i+1);
+
             int max_len = max(odd, even);
 
-            if (max_len > end - start) {
+            if(max_len > end - start)
+            {
                 start = i - (max_len - 1) / 2;
                 end = i + max_len / 2;
             }
         }
 
-        return s.substr(start, end - start + 1);        
+        return s.substr(start, end - start + 1);
+
     }
 
-private:
-    int expandAroundCenter(string s, int left, int right) {
-        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+
+    int expandAroundCenter(string s, int left, int right)
+    {
+        // this is the palindrome condition man 
+        while(left >= 0 && right < s.length() && s[left] == s[right])
+        {
             left--;
             right++;
         }
+
+        // the length of the window and stuf like that 
         return right - left - 1;
-    }    
+    }
+
 };
