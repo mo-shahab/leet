6e7d99a56e911a5992e80602a2c22b42c31c14c3
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2026-02-05 20:35:15 +0530

    0647-PALINDROMIC-SUBSTRINGS Stats: Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        return tabulation(s);
-    }
-
-    int tabulation(string &s) {
-        // matrix of size s.size() x s.size()
-        vector<vector<int>> tab(s.size(), vector<int>(s.size()));
-        
+        vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), false));
         int count = 0;
 
-        // bottom up approach
-        for(int i = s.size() - 1; i >= 0; i--){
-            for(int j = i; j < s.size(); j++){
-                if(i == j){
-                    tab[i][j] = 1;
-                }
-                else if (i + 1 == j){
-                    tab[i][j] = s[i] == s[j] ? 1 : 0;
-                }
-                else {
-                    tab[i][j] = s[i] == s[j] ? tab[i+1][j-1] : 0;
-                }
 
-                count += tab[i][j];
+        for(int i = s.size() - 1; i >= 0; i--)
+        {
+            for(int j = 0; j < s.size(); j++)
+            {
+                if(i == j) dp[i][j] = true;
+
+                else if(i + 1 == j) dp[i][j] = s[i] == s[j] ? true : false;
+
+                else {
+                    dp[i][j] = (s[i] == s[j] && dp[i+1][j-1] );
+                }
+            }
+        }
+
+        for(int i = 0; i < s.size(); i++)
+        {
+            for(int j = 0; j < s.size(); j++)
+            {
+                if(dp[i][j]) count++;
             }
         }
 
