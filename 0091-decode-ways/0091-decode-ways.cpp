0fe4c924fc57893dff1bb0a99b92827608d68e32
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2026-02-08 20:39:07 +0530

    0091-DECODE-WAYS Stats: Time: 2 ms (24.61%), Space: 9.1 MB (35%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        vector<int> memo(s.size() + 1, 0);
-        int res = 0;
-        res = recursion(s, 0, memo);      
-        return res;
-    }
-
-    int recursion(string s, int pos, vector<int> &memo){
-        if (pos == s.size()) return 1;
-        if (s[pos] == '0') return 0;
-        if (pos == s.size() -1) return 1;
-        if (memo[pos] > 0) return memo[pos];
-
-        string b = s.substr(pos, 2);
-
-        int way1 = recursion(s, pos+1, memo);
-        int way2 = 0;
-
-        if(stoi(b) <= 26 && stoi(b) > 0) {
-            way2 = recursion(s, pos+2, memo);
+        if(s[0] == '0') {
+            return 0;
         }
 
-        memo[pos] = way1 + way2;
-        return memo[pos];
+        int n =  s.length();
+
+        vector<int> dp(n + 1, 0);
+        dp[0] = dp[1] = 1; // one way to decode for 0 characters and 1 character
+
+        for(int i = 2; i <= n; i++) {
+            int one = s[i-1] - '0'; // previous character
+            int two = stoi(s.substr(i-2, 2)); // previous two characters
+
+            if(1 <= one && one <= 9) {
+                dp[i] += dp[i-1];
+            }
+
+            if(10 <= two && two < 27) {
+                dp[i] += dp[i-2];
+            }
+        }
+
+        return dp[n];
     }
 };
