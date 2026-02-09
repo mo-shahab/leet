1d459f14e252c469d3547c316ff3454f4fcda2b7
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2026-02-09 20:52:21 +0530

    0322-COIN-CHANGE Stats: Time: 15 ms (91.93%), Space: 18 MB (71.11%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,35 +1,23 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) 
-    {
-        int dp[amount + 1];
-        dp[0] = 0;
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1);
 
-        sort(begin(coins), end(coins));
+        dp[0] = 0;
 
         for(int i = 1; i <= amount; i++)
         {
             dp[i] = INT_MAX;
 
-            for(int coin: coins) 
-            {
-               /**
-                * i -> current amount
-                * coin -> current coin
-                * checking if the current coin is greater than the current amount
-                * why ? because you can only make change for the amount if the denomination
-                * in the coin is lesser than the amount ! right  
-                */
+            for(int coin: coins) {
                 if(i - coin >= 0)
                 {
-                    // this just checks if the subproblem is already solved ? 
-                    if(dp[i - coin] != INT_MAX) dp[i] = min(dp[i], dp[i - coin] + 1);
-                } 
+                    if(dp[i - coin]  != INT_MAX) dp[i] = min(dp[i], dp[i - coin] + 1);
+                }
             }
         }
 
         if(dp[amount] == INT_MAX) return -1;
         return dp[amount];
     }
-
 };
