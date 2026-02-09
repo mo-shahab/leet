12338fc6bae4450cfabd3c832fbe1ddde347fc18
Mohammed Shahabuddin Soharwardi <98043363+mo-shahab@users.noreply.github.com>
2026-02-09 20:27:03 +0530

    0091-DECODE-WAYS Stats: Time: 0 ms (100%), Space: 9.1 MB (29.12%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        if(s[0] == '0') {
-            return 0;
-        }
+        if(s[0] == '0') return 0;
 
-        int n =  s.length();
+        vector<int> dp(s.length() + 1);
 
-        vector<int> dp(n + 1, 0);
-        dp[0] = dp[1] = 1; // one way to decode for 0 characters and 1 character
+        dp[0] = 1;
+        dp[1] = 1;
 
-        for(int i = 2; i <= n; i++) {
+        for(int i = 2; i <= s.length(); i++)
+        {
             int one = s[i-1] - '0'; // previous character
             int two = stoi(s.substr(i-2, 2)); // previous two characters
 
-            if(1 <= one && one <= 9) {
-                dp[i] += dp[i-1];
-            }
-
-            if(10 <= two && two < 27) {
-                dp[i] += dp[i-2];
-            }
+            if(1 <= one && one <= 9) dp[i] += dp[i - 1];
+            if(10 <= two && two <= 26) dp[i] += dp[i-2];
         }
 
-        return dp[n];
+        return dp[s.length() ];
     }
 };
